5fc845bb0ef91b538f690c010e55556a11aecb66
antoniomejias <antonio@hostienda.com>
2016-05-26 11:55:42 -0430

    rollback

--- www/source/components/login/LoginController_2.js
+++ www/source/components/login/LoginController_2.js
@@ -3,18 +3,18 @@
 
 angular
     .module('lilotech')
-    .controller("LoginController", LoginController);
+    .controller("LoginController_2", LoginController_2);
 
-LoginController.$inject = ['LoginService', '$state', '$scope'];
+LoginController_2.$inject = ['LoginService_2', '$state' ,'$scope'];
 
-function LoginController(LoginService, $state, $scope) {
+function LoginController_2(LoginService_2, $state, $scope) {
 
     var vm = this;
     constructor();
 
     function constructor() {
         var manillaElement;
-        vm.user = {};
+
         vm.animate = {
             "cerradura": '',
             "puerta": '',
@@ -27,34 +27,18 @@ function LoginController(LoginService, $state, $scope) {
     }
 
     function _onClickLogin() {
-        LoginService
-            .validateEmptyInput(vm.user)
-            .then(function(success) { //success
-                console.log("campos bien");
-                vm.animate.manilla = 'manillaAnimate';
-            }, function(response) { // error
-                /*$cordovaToast.show(response.message, 'short', 'bottom').then(function(success) {
-                    console.log("The toast was shown");
-                }, function(error) {
-                    console.log("The toast was not shown due to " + error);
-                });*/
 
-                console.log(response.message);
-            })
-
-
-
+        vm.animate.manilla = 'manillaAnimate';   
     }
 
-    function _login() {
-        LoginService.login({
+    function _login(){
+        LoginService_2.login({
             "username": vm.username,
             "password": vm.password
         }).then(
             function(success) {
-                console.log(success);
                 $state.go('home');
-
+                
             },
             function(error) {
                 console.log(error);
@@ -65,10 +49,10 @@ function LoginController(LoginService, $state, $scope) {
     function _animateTheDoor(argument) {
         console.log("termino la transition");
         $scope.$apply(function() {
-            vm.animate.puerta = "openingDoor";
+            //vm.animate.puerta = "openingDoor";
             setTimeout(function() {
                 $scope.$apply(function() {
-                    _login()
+                    //_login()
                 });
             }, 750)
         });
